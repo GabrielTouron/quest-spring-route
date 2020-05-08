05b7dcc8482cf568b52c6006c64ac9639070ea97
Gabriel Touron <gtcode@pm.me>
2020-05-08 18:26:09 +0200

    Exercice solved

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,28 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Object doctor(@PathVariable("number") int number) {
+        Doctor doc = new Doctor();
+        if(number == 13) {
+            doc.setName("Jodie Whittaker");
+            doc.setNumber(13);
+            return doc;
+        } else if(number >=1 && number <= 12) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+        }
     }
 }
